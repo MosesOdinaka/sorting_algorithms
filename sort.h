b27f576e399ae6893ac59850fc47b74762c4ca88
MosesOdinaka <iriele.moses@gmail.com>
2023-07-25 06:43:17 -0700

    Header file

--- sort.h
+++ sort.h
@@ -5,23 +5,6 @@
 #include <stdlib.h>
 #include <string.h>
 
-void print_array(const int *array, size_t size);
-void print_list(const listint_t *list);
-void bubble_sort(int *array, size_t size);
-void insertion_sort_list(listint_t **list);
-void selection_sort(int *array, size_t size);
-void quick_sort(int *array, size_t size);
-void shell_sort(int *array, size_t size);
-void cocktail_sort_list(listint_t **list);
-void counting_sort(int *array, size_t size);
-void merge_sort(int *array, size_t size);
-void heap_sort(int *array, size_t size);
-void radix_sort(int *array, size_t size);
-void bitonic_sort(int *array, size_t size);
-void quick_sort_hoare(int *array, size_t size);
-void sort_deck(deck_node_t **deck);
-
-
 /**
  * struct listint_s - Doubly linked list node
  *
@@ -29,7 +12,6 @@ void sort_deck(deck_node_t **deck);
  * @prev: Pointer to the previous element of the list
  * @next: Pointer to the next element of the list
  */
-
 typedef struct listint_s
 {
 	const int n;
@@ -44,6 +26,23 @@ typedef struct listint_s
  */
 typedef enum bool
 {
-	false = 0,
-	true
+		false = 0,
+		true
 } bool;
+
+void print_array(const int *array, size_t size);
+void print_list(const listint_t *list);
+void bubble_sort(int *array, size_t size);
+void insertion_sort_list(listint_t **list);
+void selection_sort(int *array, size_t size);
+void quick_sort(int *array, size_t size);
+void shell_sort(int *array, size_t size);
+void cocktail_sort_list(listint_t **list);
+void counting_sort(int *array, size_t size);
+void merge_sort(int *array, size_t size);
+void heap_sort(int *array, size_t size);
+void radix_sort(int *array, size_t size);
+void bitonic_sort(int *array, size_t size);
+void quick_sort_hoare(int *array, size_t size);
+
+#endif
